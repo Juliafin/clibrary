199a3d5aa523874a49996e842d673029ad963c80
Julia Finarovsky <juliafinarovsky@gmail.com>
2017-11-17 02:06:46 -0500

    set up basic server and mongoose connection

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 require('dotenv').config({path: './backend/config_variables/.env'});
-
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
 
 app.use(bodyParser.json());
 
@@ -19,18 +22,43 @@ app.post ('/sendtest', (req, res) => {
   return res.json (req.body);
 })
 
+let server;
 
-const runServer = (port = process.env.SERVER_PORT) => {
+const runServer = (port = process.env.SERVER_PORT, dbURL = process.env.MONGO_URL) => {
   return new Promise ((resolve, reject) => {
 
-    app.listen(port, () => {
+    server = app.listen(port, () => {
       console.log(`Server running on port ${port}`);
     })
+
+    mongoose.connect(dbURL, {useMongoClient:true})
+    const dbConnection = mongoose.connection;
+  
+    dbConnection.on('error', (err) => {
+      console.log('The Mongoose connection failed with this error: ', err);
+      console.log('Closing server due to mongoose connection error');
+      server.close();
+    } )
+    dbConnection.once('open', () => {
+      console.log('Mongoose connection successful');
+    })
+
+
     
   })
 };
 
+
+// const mongooseConnection = (dbURL = process.env.MONGO_URL) => {
+  
+// }
+
+
+
 runServer()
+.then(() => {
+  console.log('testing')
+})
 .catch((err) => {
   console.log('Error with the server starting', err);
 })
